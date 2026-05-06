f1076fe17f8589edf38f274ec25edb7072b8d3fd
hanumanthabd <hanumanthabd17@gmail.com>
2026-05-06 22:13:54 +0530

    Create icg.c

---
+++ src/src/src/src/src/src/src/icg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "icg.h"
+
+void generateICG() {
+
+    printf("\nThree Address Code:\n");
+
+    printf("t1 = a + b\n");
+    printf("t2 = t1 * c\n");
+    printf("x = t2\n");
+}
